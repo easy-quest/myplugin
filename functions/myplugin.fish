aabf5404fc5e63478ecf2e6f2b38551ab9966140
Ivan Yastrebov <easy-quest@mail.ru>
2021-10-31 14:06:40 +0300

    first

---
+++ functions/myplugin.fish
@@ -0,0 +1,19 @@
+function myplugin -d "My package"
+# 
+   # if  python3 -m venv .venv
+        # echo "install"
+   # end
+    # 
+    # if direnv -f $PREFIX/bin/direnv
+       # direnv allow .
+    # else
+        # python3 -m pip install -U direnv;direnv allow .
+    # end
+# 
+   # ./.venv/bin/python -m pip install -U pip setuptools wheel pip-tools
+# 
+  # # Package entry-point
+  echo Hello $argv
+end
+# 
+# 
---
+++ init.fish
@@ -0,0 +1,6 @@
+# myplugin initialization hook
+#
+# You can use the following variables in this file:
+* $package       myplugin
+* $path          /data/data/com.termux/files/home/.config/omf/pkg/myplugin
+* $dependencies  myplugin
